6adabb848dceeaa9fc784ffe3fecb0d8a8122cd6
Lusca Lima <lusca.lima@outlook.com>
2021-09-19 21:56:41 -0300

    chore: add vue-icons

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import store from './store'
 // Style
 import './style/reset.scss'
 
+// Icons
+import icons from './assets/icons'
+
 createApp(App)
   .use(store)
   .use(router)
+  .use(icons)
   .mount('#app')
