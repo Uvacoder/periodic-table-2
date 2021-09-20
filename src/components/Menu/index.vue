90ab7e5ab35fc7a2196fa4ec9295da687a38d41f
Lusca Lima <lusca.lima@outlook.com>
2021-09-20 20:50:36 -0300

    feat: create menu component

--- src/components/Menu/index.vue
+++ src/components/Menu/index.vue
@@ -1,18 +1,69 @@
 <template>
-  <sidebar class="menu"> </sidebar>
+  <sidebar class="menu" :class="additionalClasses">
+    <header class="menu__header">
+      <span class="menu__toggle" @click="toggleMenu">
+        <unicon :name="defineToggleIcon" />
+      </span>
+    </header>
+
+    <ul class="menu__items"></ul>
+  </sidebar>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed, reactive } from 'vue'
+
+const state = reactive({
+  isOpen: false
+})
+
+const defineToggleIcon = computed(() => (state.isOpen ? 'multiply' : 'bars'))
+const additionalClasses = computed(() => ({
+  'menu--open': state.isOpen
+}))
+
+function toggleMenu() {
+  state.isOpen = !state.isOpen
+}
+</script>
 
 <style scoped lang="scss">
 @use '~@/style/variables';
 
 .menu {
-  width: 3rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   height: calc(100vh - 1rem);
   margin: 0.5rem;
+  padding: 0.75rem;
   border-radius: variables.$radius;
-  background-color: variables.$light;
+  background-color: #fff;
   box-shadow: variables.$default-shadow;
+  user-select: none;
+
+  &__header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 1rem;
+  }
+
+  &__toggle {
+    cursor: pointer;
+  }
+
+  &--open {
+    width: 300px;
+  }
+
+  &--open &__header {
+    justify-content: flex-end;
+  }
+
+  &__items {
+    width: 100%;
+  }
 }
 </style>
