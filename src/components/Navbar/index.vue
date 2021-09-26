42d96fd787b35c3d1931289ebaf1f8545dd8adbb
Lusca Lima <lusca.lima@outlook.com>
2021-09-26 11:28:52 -0300

    feat: create the navbar

---
+++ src/components/Navbar/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar">
+    <h1 class="navbar__title">
+      Periodic Table of Chemical Elements
+    </h1>
+    <Search />
+  </nav>
+</template>
+
+<script setup>
+// Components
+import Search from './Search.vue'
+</script>
+
+<style scoped lang="scss">
+@use '~@/style/variables';
+
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+
+  &__title {
+    font-family: variables.$default-font;
+    text-transform: uppercase;
+    line-height: 1;
+  }
+}
+</style>
