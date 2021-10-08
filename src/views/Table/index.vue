2b2e268a8b47e4d74082897484303490ab814f7d
Lusca Lima <lusca.lima@outlook.com>
2021-10-07 21:20:02 -0300

    refactor: makes the element's color self-contained

--- src/views/Table/index.vue
+++ src/views/Table/index.vue
@@ -14,8 +14,8 @@
           :number="element.number"
           :symbol="element.symbol"
           :name="element.name"
+          :category="defineCategory(element.category)"
           :has-offset="hasOffsetByNumber(element.number)"
-          :color="categoryToColor(element.category)"
           :is-selected="isSelected(element)"
         />
       </div>
@@ -32,8 +32,8 @@
             :number="element.number"
             :symbol="element.symbol"
             :name="element.name"
+            :category="defineCategory(element.category)"
             :has-offset="!elIndex"
-            :color="categoryToColor(element.category)"
             :is-selected="isSelected(element)"
           />
         </div>
@@ -54,18 +54,6 @@ import Element from '@/components/Element'
 import { isSelected } from './composables/selection'
 
 const ELEMENTS_NUMBERS_OFFSET = [2, 5, 13, 72, 104]
-const CATEGORY_COLORS = {
-  noble_gas: '#93ceed',
-  alkali_metal: '#ff6666',
-  alkaline_earth_metal: '#fedead',
-  metalloid: '#cccc9a',
-  nonmetal: '#f1ff90',
-  halogen: '#c1feff',
-  post_transition_metal: '#cccccc',
-  transition_metal: '#ffc0bf',
-  lanthanide: '#ffbffe',
-  actinide: '#ff99cb'
-}
 
 const seriesElements = [elements.slice(56, 71), elements.slice(88, 103)]
 const commomElements = [
@@ -82,10 +70,8 @@ function hasOffsetByNumber(number) {
   return ELEMENTS_NUMBERS_OFFSET.includes(number)
 }
 
-function categoryToColor(rawCategory) {
-  const category = rawCategory.replace(/-|\s|,/gi, '_')
-
-  return CATEGORY_COLORS[category]
+function defineCategory(rawCategory) {
+  return rawCategory.replace(/-|\s|,/gi, '_')
 }
 </script>
 
